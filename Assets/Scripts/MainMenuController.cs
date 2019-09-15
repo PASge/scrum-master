69d84ef146a5181729ae24ed9c21f93bc4a536e9
aatkin <atkinart@gmail.com>
2019-09-15 22:12:47 +0300

    add menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("FirstLevelScene");
+    }
+
+    public void options()
+    {
+
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
